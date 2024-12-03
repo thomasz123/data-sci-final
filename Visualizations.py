4df9e5f204239ecaf20e6466bf34b2d19f666efe
thomasz123 <94854167+thomasz123@users.noreply.github.com>
2024-12-03 23:47:55 +0000

    edits

--- Visualizations.py
+++ Visualizations.py
@@ -20,16 +20,16 @@ st.title(":red[Visualizations]")
 
 tab1, tab2, tab3, tab4= st.tabs(["Count Plots", "Box and Whisker Plots", "Pie Charts", "Map"])
 
-@st.cache_data 
-def pairplot():
-    st.pyplot(sns.pairplot(df))
+# @st.cache_data 
+# def pairplot():
+#     st.pyplot(sns.pairplot(df))
 
-@st.cache_data 
-def heatmap():
-    corr_matrix= df.corr()
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax, linewidths=0.5)
-    st.pyplot(fig)
+# @st.cache_data 
+# def heatmap():
+#     corr_matrix= df.corr()
+#     fig, ax = plt.subplots(figsize=(10, 6))
+#     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax, linewidths=0.5)
+#     st.pyplot(fig)
     #return fig
 
 # @st.cache_data
@@ -49,23 +49,23 @@ with tab1: #count plots
 
 with tab2: #box and whisker plots
     st.header("Scatterplot")
-    variable = st.radio("Pick one", cols)
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.scatterplot(data = df, x = variable, y = "Injury Severity")
-    st.pyplot(fig)
+    # variable = st.radio("Pick one", cols)
+    # fig, ax = plt.subplots(figsize=(10, 6))
+    # sns.scatterplot(data = df, x = variable, y = "Injury Severity")
+    # st.pyplot(fig)
     st.markdown("You can choose a variable to see its scatterplot with temperature. We can see from the scatterplot if there's any semblance of correlation between the variable and temperature.")
 
 with tab3: #pie charts
     with st.spinner("Loading visualizations..."):
         st.header('Pairplot')
-        pairplot()
+        # pairplot()
         st.markdown("This pairplot shows the scatterplot between any two variables. We get a more full idea of the correlations between certain variables.  The diagonal shows the countplot for that variable, and shows the distribution of the data for that variable.")
 
 
 with tab4: #map
     with st.spinner("Loading visualizations..."):
         st.header('Heatmap')
-        heatmap()
+        # heatmap()
         st.markdown("The heatmap shows the correlation value between any variables. The closer the value is to 1, the greater the correlation, and if the value is negative, the correlation is negative. We can see that dew and temperature have a correlation of 0.87, while humidiy and solar radiation have a correlation of -0.7.")
 
 
